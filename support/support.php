fad8eaf16fd98b43f5a17a12d8ea5fef05a3c4b1
namith.jawahar <namith.jawahar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-05 11:38:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-insert/tags/1.0@115696 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ support/support.php
@@ -0,0 +1,20 @@
+<?php function show_support_options() { ?>
+<table class="form-table">
+<tr>
+<td align="left">
+<a href="http://www.smartlogix.co.in/"><img src="<?php echo get_bloginfo('url') ?>/wp-content/plugins/wp-insert/images/Smart-Logix-we-customize.jpg" /></a>
+</td>
+<td align="right">
+<b>Think we have done a great job?</b><br/><a href="http://wordpress.org/extend/plugins/wp-insert/">Rate the plugin</a> or <a href="http://www.smartlogix.co.in/">Leave Us a Comment</a>
+<div>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="5199131">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+</div>
+</td>
+</tr>
+</table>
+<?php } ?>
