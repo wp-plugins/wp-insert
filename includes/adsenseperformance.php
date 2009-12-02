83b7b3708ab11b836bf382e6690d4dc161666332
namith.jawahar <namith.jawahar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-02 15:09:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-insert/tags/1.5.1@178803 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/adsenseperformance.php
@@ -0,0 +1,45 @@
+<?php
+require_once (dirname(__FILE__) . '/adsense.php');
+
+if(get_option('wp_insert_adsense_username') != '') {
+$adsense = new AdSense();
+//if ($adsense->connect('namith.jawahar@gmail.com', 'harikrishnan')) { echo 'Connected Successfully'; }
+}
+
+function wp_insert_adsense_page() {
+	global $screen_layout_columns;
+
+	add_meta_box('wp_insert_adsense_login_credentials', 'Adsense Username and Password', 'wp_insert_adsense_login_credentials_HTML', 'col_1');
+	add_meta_box('wp_insert_adsense_sincelastpayment', 'Adsense : Total Since Last Payment', 'wp_insert_adsense_sincelastpayment_HTML', 'col_1');
+	/*add_meta_box('wp_insert_adsense_today', 'Adsense : Today', 'wp_insert_adsense_today_HTML', 'col_1');
+	add_meta_box('wp_insert_adsense_yesterday', 'Adsense : Yesterday', 'wp_insert_adsense_yesterday_HTML', 'col_1');
+	add_meta_box('wp_insert_adsense_last7days', 'Adsense : Last 7 Days', 'wwp_insert_adsense_last7days', 'col_1');
+	add_meta_box('wp_insert_adsense_lastmonth', 'Adsense : Last Month', 'wp_insert_adsense_lastmonth_HTML', 'col_1');
+	add_meta_box('wp_insert_adsense_thismonth', 'Adsense : This Month', 'wp_insert_adsense_thismonth_HTML', 'col_1');*/
+
+	$parameters = 'wp_insert_adsense_username, wp_insert_adsense_password';
+	wp_insert_settings_page_layout($parameters, 'WP-INSERT : Monitor Adsense', 'adsense');
+}
+
+function wp_insert_adsense_login_credentials_HTML() { ?>
+<div>
+<p>
+<label for="wp_insert_adsense_username">Adsense Username :</label>
+<input id="wp_insert_adsense_username" class="widefat" type="text" value="<?php echo get_option('wp_insert_adsense_username'); ?>" name="wp_insert_adsense_username"/>
+</p>
+<p>
+<label for="wp_insert_adsense_password">Adsense Password:</label>
+<input id="wp_insert_adsense_password" class="widefat" type="password" value="<?php echo get_option('wp_insert_adsense_password'); ?>" name="wp_insert_adsense_password"/>
+</p>
+<p>
+<small>Data Retrieval Based on <a href="http://www.webtoolkit.info/php-adsense-account-monitor.html">PHP AdSense account monitor class</a></small>
+</p>
+</div>
+<?php }
+
+function wp_insert_adsense_sincelastpayment_HTML() { ?>
+<div>
+
+</div>
+<?php }
+?>
---
+++ includes/inpostads.php
@@ -0,0 +1,269 @@
+<?php
+function wp_insert_filter_content_ads($content, $ad_ID) {
+	$output = '';
+	global $random;
+	$page_details = wp_insert_get_current_page_details();
+	
+	if(get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_enable')) {
+		/*Finding a appropriate position near the middle of the content to insert the ad*/
+		$positions = array();
+		$endpos = -1;
+		$trigger = "<p";
+		if(strpos($content, "<p") === false)
+		$trigger = "<br";
+
+		while(strpos($content, $trigger, $endpos+1) !== false){
+		$endpos = strpos($content, $trigger, $endpos+1);
+		$positions[] = $endpos;
+		}
+		$middle = sizeof($positions);
+		while(sizeof($positions) > $middle)
+		array_pop($positions);
+		$triggerpoint = $positions[floor(sizeof($positions)/2)];
+		/*End of Finding a appropriate position near the middle of the content to insert the ad*/
+
+		if ($middle > 2) {
+			$ad_style = get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_style');
+			if($ad_style == '') { $ad_style = 'margin: 5px; padding: 0px;'; }
+			if(!get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_exclude_home') && ($page_details == 'HOME')) {
+				switch($random) {
+					case 0:
+						$content = substr_replace($content, '<div id="in_post_ad_middle_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_content').'</div>'.$trigger, $triggerpoint, 2);
+					break;
+					case 1:
+						$content = substr_replace($content, '<div id="in_post_ad_middle_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_content_1').'</div>'.$trigger, $triggerpoint, 2);
+					break;
+					case 2:
+						$content = substr_replace($content, '<div id="in_post_ad_middle_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_content_2').'</div>'.$trigger, $triggerpoint, 2);
+					break;
+				}
+			}
+			else if(!get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_exclude_archives') && ($page_details == 'ARCHIVE')) {
+				switch($random) {
+					case 0:
+						$content = substr_replace($content, '<div id="in_post_ad_middle_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_content').'</div>'.$trigger, $triggerpoint, 2);
+					break;
+					case 1:
+						$content = substr_replace($content, '<div id="in_post_ad_middle_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_content_1').'</div>'.$trigger, $triggerpoint, 2);
+					break;
+					case 2:
+						$content = substr_replace($content, '<div id="in_post_ad_middle_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_content_2').'</div>'.$trigger, $triggerpoint, 2);
+					break;
+				}
+			}
+			else {
+				if(is_singular()) {
+					$page_ids = explode(',', str_replace(" ", "", get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_exclude_ids')));
+					if(wp_insert_is_in_array($page_details, $page_ids) =='FALSE') {
+						switch($random) {
+							case 0:
+								$content = substr_replace($content, '<div id="in_post_ad_middle_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_content').'</div>'.$trigger, $triggerpoint, 2);
+							break;
+							case 1:
+								$content = substr_replace($content, '<div id="in_post_ad_middle_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_content_1').'</div>'.$trigger, $triggerpoint, 2);
+							break;
+							case 2:
+								$content = substr_replace($content, '<div id="in_post_ad_middle_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_middle_'.$ad_ID.'_content_2').'</div>'.$trigger, $triggerpoint, 2);
+							break;
+						}						
+					}
+				}
+			}
+		}
+	}
+	if(get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_enable')) {
+		$ad_style = get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_style');
+		if($ad_style == '') { $ad_style = 'margin: 5px; padding: 0px;'; }
+		if(!get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_exclude_home') && ($page_details == 'HOME')) {
+			switch($random) {
+				case 0:
+					$output .= '<div id="in_post_ad_top_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_content').'</div>';
+				break;
+				case 1:
+					$output .= '<div id="in_post_ad_top_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_content_1').'</div>';
+				break;
+				case 2:
+					$output .= '<div id="in_post_ad_top_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_content_2').'</div>';
+				break;
+			}			
+		}
+		else if(!get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_exclude_archives') && ($page_details == 'ARCHIVE')) {
+			switch($random) {
+				case 0:
+					$output .= '<div id="in_post_ad_top_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_content').'</div>';
+				break;
+				case 1:
+					$output .= '<div id="in_post_ad_top_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_content_1').'</div>';
+				break;
+				case 2:
+					$output .= '<div id="in_post_ad_top_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_content_2').'</div>';
+				break;
+			}			
+		}
+		else {
+			if(is_singular()) {
+				$page_ids = explode(',', str_replace(" ", "", get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_exclude_ids')));
+				if(wp_insert_is_in_array($page_details, $page_ids) =='FALSE') {
+					switch($random) {
+						case 0:
+							$output .= '<div id="in_post_ad_top_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_content').'</div>';
+						break;
+						case 1:
+							$output .= '<div id="in_post_ad_top_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_content_1').'</div>';
+						break;
+						case 2:
+							$output .= '<div id="in_post_ad_top_'.$ad_ID.'" style="'.$ad_style.'">'.get_option('wp_insert_in_post_ad_top_'.$ad_ID.'_content_2').'</div>';
+						break;
+					}					
+				}
+			}
+		}
+	}
+	if(get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_enable')) {
+		$ad_style = get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_style');
+		if($ad_style == '') { $ad_style = 'margin: 5px; padding: 0px;'; }
+		if(!get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_exclude_home') && ($page_details == 'HOME')) {
+			switch($random) {
+				case 0:
+					$output .= '<div id="in_post_ad_left_'.$ad_ID.'" style="float:left;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_content').'</div>';
+				break;
+				case 1:
+					$output .= '<div id="in_post_ad_left_'.$ad_ID.'" style="float:left;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_content_1').'</div>';
+				break;
+				case 2:
+					$output .= '<div id="in_post_ad_left_'.$ad_ID.'" style="float:left;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_content_2').'</div>';
+				break;
+			}			
+		}
+		else if(!get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_exclude_archives') && ($page_details == 'ARCHIVE')) {
+			switch($random) {
+				case 0:
+					$output .= '<div id="in_post_ad_left_'.$ad_ID.'" style="float:left;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_content').'</div>';
+				break;
+				case 1:
+					$output .= '<div id="in_post_ad_left_'.$ad_ID.'" style="float:left;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_content_1').'</div>';
+				break;
+				case 2:
+					$output .= '<div id="in_post_ad_left_'.$ad_ID.'" style="float:left;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_content_2').'</div>';
+				break;
+			}			
+		}
+		else {
+			if(is_singular()) {
+				$page_ids = explode(',', str_replace(" ", "", get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_exclude_ids')));
+				if(wp_insert_is_in_array($page_details, $page_ids) =='FALSE') {
+					switch($random) {
+						case 0:
+							$output .= '<div id="in_post_ad_left_'.$ad_ID.'" style="float:left;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_content').'</div>';
+						break;
+						case 1:
+							$output .= '<div id="in_post_ad_left_'.$ad_ID.'" style="float:left;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_content_1').'</div>';
+						break;
+						case 2:
+							$output .= '<div id="in_post_ad_left_'.$ad_ID.'" style="float:left;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_left_'.$ad_ID.'_content_2').'</div>';
+						break;
+					}					
+				}
+			}
+		}
+	}
+	if(get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_enable')) {
+		$ad_style = get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_style');
+		if($ad_style == '') { $ad_style = 'margin: 5px; padding: 0px;'; }
+		if(!get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_exclude_home') && ($page_details == 'HOME')) {
+			switch($random) {
+				case 0:
+					$output .= '<div id="in_post_ad_right_'.$ad_ID.'" style="float:right;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_content').'</div>';
+				break;
+				case 1:
+					$output .= '<div id="in_post_ad_right_'.$ad_ID.'" style="float:right;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_content_1').'</div>';
+				break;
+				case 2:
+					$output .= '<div id="in_post_ad_right_'.$ad_ID.'" style="float:right;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_content_2').'</div>';
+				break;
+			}			
+		}
+		else if(!get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_exclude_archives') && ($page_details == 'ARCHIVE')) {
+			switch($random) {
+				case 0:
+					$output .= '<div id="in_post_ad_right_'.$ad_ID.'" style="float:right;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_content').'</div>';
+				break;
+				case 1:
+					$output .= '<div id="in_post_ad_right_'.$ad_ID.'" style="float:right;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_content_1').'</div>';
+				break;
+				case 2:
+					$output .= '<div id="in_post_ad_right_'.$ad_ID.'" style="float:right;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_content_2').'</div>';
+				break;
+			}			
+		}
+		else {
+			if(is_singular()) {
+				$page_ids = explode(',', str_replace(" ", "", get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_exclude_ids')));
+				if(wp_insert_is_in_array($page_details, $page_ids) =='FALSE') {
+					switch($random) {
+						case 0:
+							$output .= '<div id="in_post_ad_right_'.$ad_ID.'" style="float:right;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_content').'</div>';
+						break;
+						case 1:
+							$output .= '<div id="in_post_ad_right_'.$ad_ID.'" style="float:right;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_content_1').'</div>';
+						break;
+						case 2:
+							$output .= '<div id="in_post_ad_right_'.$ad_ID.'" style="float:right;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_right_'.$ad_ID.'_content_2').'</div>';
+						break;
+					}					
+				}
+			}
+		}
+	}
+	if(get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_enable')) {
+		$ad_style = get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_style');
+		if($ad_style == '') { $ad_style = 'margin: 5px; padding: 0px;'; }
+		if(!get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_exclude_home') && ($page_details == 'HOME')) {
+			switch($random) {
+				case 0:
+					$content .= '<div id="in_post_ad_bottom_'.$ad_ID.'" style="clear:both;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_content').'</div>';
+				break;
+				case 1:
+					$content .= '<div id="in_post_ad_bottom_'.$ad_ID.'" style="clear:both;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_content_1').'</div>';
+				break;
+				case 2:
+					$content .= '<div id="in_post_ad_bottom_'.$ad_ID.'" style="clear:both;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_content_2').'</div>';
+				break;
+			}			
+		}
+		else if(!get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_exclude_archives') && ($page_details == 'ARCHIVE')) {
+			switch($random) {
+				case 0:
+					$content .= '<div id="in_post_ad_bottom_'.$ad_ID.'" style="clear:both;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_content').'</div>';
+				break;
+				case 1:
+					$content .= '<div id="in_post_ad_bottom_'.$ad_ID.'" style="clear:both;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_content_1').'</div>';
+				break;
+				case 2:
+					$content .= '<div id="in_post_ad_bottom_'.$ad_ID.'" style="clear:both;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_content_2').'</div>';
+				break;
+			}			
+		}
+		else {
+			if(is_singular()) {
+				$page_ids = explode(',', str_replace(" ", "", get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_exclude_ids')));
+				if(wp_insert_is_in_array($page_details, $page_ids) =='FALSE') {
+					switch($random) {
+						case 0:
+							$content .= '<div id="in_post_ad_bottom_'.$ad_ID.'" style="clear:both;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_content').'</div>';
+						break;
+						case 1:
+							$content .= '<div id="in_post_ad_bottom_'.$ad_ID.'" style="clear:both;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_content_1').'</div>';
+						break;
+						case 2:
+							$content .= '<div id="in_post_ad_bottom_'.$ad_ID.'" style="clear:both;'.$ad_style.'">'.get_option('wp_insert_in_post_ad_bottom_'.$ad_ID.'_content_2').'</div>';
+						break;
+					}					
+				}
+			}
+		}
+	}
+	$content = $output.$content."<div style='clear:both'></div>";
+	return $content;
+}
+?>
