a0d69241ea83a07b96e837fa8495bbcd0afed948
namith.jawahar <namith.jawahar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-12 04:36:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-insert/tags/1.7.5@530142 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 1.4/js/postpicker.js.php
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-function ShowPopUp() {
-	GreyOutScreen();
-    divPopup = CreatePopUp("Add New Folder", 285, 141, 100, 200);
-    divPopup.innerHTML = <?php wp_list_pages('echo = 0'); ?>;
- }
- </script>
