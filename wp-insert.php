f4e7aebfb3bf74494a392bd74b1a75782948ce7c
namith.jawahar <namith.jawahar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-12 04:33:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-insert/trunk@530141 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-insert.php
+++ wp-insert.php
@@ -3,7 +3,7 @@
 Plugin Name: wp-insert
 Plugin URI: http://www.wp-insert.smartlogix.co.in/
 Description: The ultimate wordpress plugin
-Version: 1.7.5
+Version: 1.7.6
 Author: Namith Jawahar
 Author URI: http://www.smartlogix.co.in/
 WP-INSERT by SMARTLOGIX : The ultimate wordpress plugin
